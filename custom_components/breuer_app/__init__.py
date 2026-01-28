6011dc872d2d1801b2c18ae123153e123aa4104c
Milad Heydari <milad.heydaryi@gmail.com>
2026-01-28 13:13:45 +0000

    fix getting default value

--- custom_components/breuer_app/__init__.py
+++ custom_components/breuer_app/__init__.py
@@ -1,6 +1,7 @@
 """Init for the breuer_app integration."""
 
 from __future__ import annotations
+import logging
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, CONF_PORT, Platform
@@ -10,17 +11,24 @@ from .api import OledModuleApi
 from .const import DEFAULT_HOST,DEFAULT_PORT
 
 _PLATFORMS: list[Platform] = [Platform.BUTTON]
-
+_LOGGER = logging.getLogger(__name__)
 type BreuerConfigEntry = ConfigEntry[OledModuleApi]
 
 async def async_setup_entry(hass: HomeAssistant, entry: BreuerConfigEntry) -> bool:
     """Set up OLED module from config entry."""
     api = OledModuleApi(
-        host=entry.data.get[CONF_HOST,DEFAULT_HOST],
-        port=entry.data.get[CONF_PORT,DEFAULT_PORT],
+        host=entry.data.get(CONF_HOST,DEFAULT_HOST),
+        port=entry.data.get(CONF_PORT,DEFAULT_PORT),
     )
 
-    await api.async_connect()
+    try:
+        await api.async_connect()
+    except ConnectionRefusedError:
+        _LOGGER.warning(
+            "Could not connect to OLED at %s:%s. Will retry when sending text.",
+            api._host,
+            api._port,
+        )
 
     entry.runtime_data = api
 
